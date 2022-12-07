42f1ae4bd2b6352bb3b9fcf165815de612a7a9bd
Justin Popa <481655+justinpopa@users.noreply.github.com>
2022-12-06 21:13:53 -0500

    Day5 solutions

--- day5/day5.py
+++ day5/day5.py
@@ -10,16 +10,33 @@ column_line = re.search('^(?:\W+\d+\W)+$', input, re.MULTILINE)
 
 columns = [index for index, char in enumerate(input[column_line.start():column_line.end()]) if char.isdigit()]
 
-raw_stacks, moves = re.split('^(?:\W+\d+\W)+\n', input, flags=re.MULTILINE)
+raw_stacks, moves = re.split('^(?:\W+\d+\W)+\n\n', input, flags=re.MULTILINE)
 
-stacks = []
+def set_columns(raw_stacks, columns):
+    tmp_stack = [[] for i in range(len(columns))]
 
-for column in columns:
-    tmp_stack = []
     for stack in raw_stacks.splitlines():
+        for c in range(len(stack)):
+            if c in columns and stack[c].isalpha():
+                tmp_stack[columns.index(c)].append(stack[c])
 
-        for c in range(0, len(stack), 1):
-            if c == column and stack[c].isalpha():
-                tmp_stack.append(stack[c])
-    stacks.append(tmp_stack)
+    return tmp_stack
 
+def move_crates(amount, source, dest, stacks):
+    crates = stacks[source - 1][0:amount]
+    stacks[dest - 1] = crates + stacks[dest - 1]
+    [stacks[source -1].remove(c) for c in crates]
+
+answer1 = set_columns(raw_stacks, columns)
+answer2 = set_columns(raw_stacks, columns)
+
+for move in moves.splitlines():
+    amount, source, dest = re.findall('(\d+) from (\d+) to (\d+)', move).pop()
+
+    for x in range(int(amount)):
+        move_crates(1, int(source), int(dest), answer1)
+
+    move_crates(int(amount), int(source), int(dest), answer2)
+
+print(''.join([i[0] for i in answer1]))
+print(''.join([i[0] for i in answer2]))
