5a3b17edb4c62ab3b9c30cd58363156999148bfb
Justin Popa <481655+justinpopa@users.noreply.github.com>
2022-12-06 23:18:38 -0500

    That was easy

--- day6/day6.py
+++ day6/day6.py
@@ -3,3 +3,11 @@ from aocd.models import Puzzle
 puzzle = Puzzle(year=2022, day=6)
 
 input = puzzle.input_data
+
+def find_first_distinct_set_of_x(string, length):
+    for i in range(len(input)):
+        if len(''.join(set(input[i - length:i]))) == length:
+            return i
+
+print(find_first_distinct_set_of_x(input, 4))
+print(find_first_distinct_set_of_x(input, 14))
